ccfa854a61a663a930cf691a9e5fb36a34a90dc3
Bianca Santana Souza <bsantana@c1r5p2.42sp.org.br>
2024-04-09 18:05:57 -0300

    pequenos ajustes no header

--- src/push_swap.h
+++ src/push_swap.h
@@ -6,46 +6,70 @@
 /*   By: bsantana <bsantana@student.42sp.org.br     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 11:40:08 by bsantana          #+#    #+#             */
-/*   Updated: 2024/04/09 16:45:19 by bsantana         ###   ########.fr       */
+/*   Updated: 2024/04/09 18:05:08 by bsantana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-/* Bibliotecas */
-# include <stdio.h>
+/**** Libraries ****/
+
 # include <unistd.h>
 # include <limits.h>
 # include <stdlib.h>
-# include <stdbool.h>
 
-/* Estrutura para armazenar dados da stack em lista linkada */
+/* Stack used for sorting */
 typedef struct s_stack
 {
 	int				value;
 	struct s_stack	*next;
 }	t_stack;
 
-/* Estrutura de dados */
+/**** Input validations ****/
+
+/**/
 int			correct_input(char **argv);
-void		error(t_stack **stack);
-void		ft_putstr_fd(char *s, int fd);
-void		free_stack(t_stack **stack);
+
+/**/
 int			only_numbers(char *str);
+
+/**/
 int			repeats_numbers(char **argv);
+
+/**/
 int			aux_repeats_numbers(const char *s1, const char *s2);
+
+/**/
 int			zero_count(char *av);
+
+/**** Stack initialization ****/
+
+/**/
 t_stack		*init_stack(int ac, char **av);
+
+/**/
 void		node_list_bottom(t_stack **stack, t_stack *new);
+
+/**/
 t_stack		*get_bottom_list(t_stack *stack);
+
+/**/
 t_stack		*add_node(int nbr);
 
-/*   *** Auxiliares ***  */
+/**** Errors and memory deallocation ****/
 
-/* Usada para pegar os valores tradizos como argumento quando
-* eles vem em um único argumento separado por espaços */
+/**/
+void		free_stack(t_stack **stack);
+
+/**/
+void		error(t_stack **stack);
+
+/**** Auxiliares ****/
+
 long int	ft_atol(const char *nptr);
-void		printf_stack(t_stack *stack_a);
+void		ft_putstr_fd(char *s, int fd);
+
+//void		printf_stack(t_stack *stack_a);
 
 #endif
