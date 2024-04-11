0eb78eed45e083109a5d60e2d7c707fd5fa90ecd
Bianca Santana Souza <bsantana@c1r5p1.42sp.org.br>
2024-04-11 14:25:16 -0300

    iniciando costrução do algoritmo

---
+++ src/indexes.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   indexes.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bsantana <bsantana@student.42sp.org.br     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/11 14:21:55 by bsantana          #+#    #+#             */
+/*   Updated: 2024/04/11 14:24:58 by bsantana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+// void	assign_indexes(t_stack *stack_a, int stack_size)
+// {
+// 	t_stack	*ptr;
+// 	t_stack	*highest_value;
+// 	int		value;
+
+// 	while ()
+// }
